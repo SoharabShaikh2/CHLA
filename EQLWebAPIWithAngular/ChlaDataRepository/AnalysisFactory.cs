a741cf32dd81e47c57508767a23ce054affa84bd
Soharab Shaikh <iamsoharab@gmail.com>
2019-03-27 22:57:49 +0530

    New Model Changes

---
+++ EQLWebAPIWithAngular/ChlaDataRepository/AnalysisFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChlaDataRepository
+{
+    class AnalysisFactory
+    {
+        public IAnalysis GetAnalysis(string scenario)
+        {
+            switch(scenario)
+            {
+                case "Scenario 1":
+                    return new Scenario1QuantitativeAnalysis();
+                                 
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ EQLWebAPIWithAngular/ChlaDataRepository/Scenario1QuantitativeAnalysis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace ChlaDataRepository
+{
+    class Scenario1QuantitativeAnalysis : Analysis
+    {
+        protected override void Initialise()
+        {
+            RegisterAnalyser(new CheckBreathAnalyser());
+        }
+    }
+}
