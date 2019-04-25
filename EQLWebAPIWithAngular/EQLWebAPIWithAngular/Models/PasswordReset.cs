665ce25b90fa11af9d009d4c4c0ea03776c24aae
Soharab Shaikh <iamsoharab@gmail.com>
2019-04-25 23:39:49 +0530

    Admin panel login issue fixed

--- EQLWebAPIWithAngular/EQLWebAPIWithAngular/Models/PasswordReset.cs
+++ EQLWebAPIWithAngular/EQLWebAPIWithAngular/Models/PasswordReset.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace EQLWebAPIWithAngular.Models
 {
     public class PasswordReset
     {
+        [Key]
+        public int Id { get; set; }
         public int Resetcode { get; set; }
         public int userid { get; set; }
         public bool status { get; set; }
