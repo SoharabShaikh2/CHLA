4df543b19f498206cc4ea813eb91d8c1ae211f66
Abhishek <abhishek.s@aisolve.com>
2019-04-25 19:43:23 +0530

    Changes

---
+++ EQLWebAPIWithAngular/EQLWebAPIWithAngular/Models/PasswordReset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EQLWebAPIWithAngular.Models
+{
+    public class PasswordReset
+    {
+        public int Resetcode { get; set; }
+        public int userid { get; set; }
+        public bool status { get; set; }
+    }
+}
